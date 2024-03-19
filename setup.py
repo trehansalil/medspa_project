71d04bfa487b62ad932283977f6ec0bd11453f29
Salil Trehan <trehansalil1@gmail.com>
2024-03-19 17:56:47 +0530

    leadboard/status/list api updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medspa_project',
+    version='0.0.1',
+    author='Salil Trehan',
+    author_email='trehansalil1@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
